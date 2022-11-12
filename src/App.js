54e82dfbd9bf84b74bdcd56273e8c54785f221f5
Thai Huynh <rafehuynh0510@gmail.com>
2022-11-12 13:42:22 -0500

    shuffer cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import Card from './Card'
+const cardValues = [{ "name": "Card1" },
+{ "name": "Card2" },
+{ "name": "Card3" },
+{ "name": "Card4" }];
+
 function App() {
-    const cardValues = ["Card1", "Card2", "Card3", "Card4"];
+    const [cards, setCards] = useState([])
+    const doublecards = [...cardValues, ...cardValues]
+    function resetGame() {
+        window.location.reload(false)
+    }
+    // shuffel the cards 
+    const shuffel = () => {
+        const shuffed = doublecards.sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }))
+        setCards(shuffed)
+    }
+    console.log(cards)
     return (
         <div className="App">
+            <button onClick={resetGame}> New Game </button>
             {cardValues.map((item, idx) => (<Card key={idx} cardText={item} />))}
         </div>
     );
