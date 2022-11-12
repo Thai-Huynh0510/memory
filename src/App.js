f296fe66f67aecdb3704a3586a859a62ccf55b8d
Thai Huynh <rafehuynh0510@gmail.com>
2022-11-12 17:56:43 -0500

    matched 2 cards or false flip back

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './Card'
 
-const cardValues = [{ "name": "Card1" },
-{ "name": "Card2" },
-{ "name": "Card3" },
-{ "name": "Card4" }];
+const cardValues = [{ "name": "Card1", status: false  },
+    { "name": "Card2", status: false },
+    { "name": "Card3", status: false },
+    { "name": "Card4", status: false }];
 
 function App() {
     const [cards, setCards] = useState([])
     const doublecards = [...cardValues, ...cardValues]
     const [Card1, setCard1] = useState(null)
     const [Card2, setCard2] = useState(null)
-    const resetGame = () => {
-        window.location.reload(true)
-    }
     // shuffel the cards 
     const shuffel = () => {
         const shuffed = doublecards.sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }))
@@ -22,17 +19,25 @@ function App() {
     }
     // handle choice for card 1 and card 2
     const handleChoice = (card) => {
-        Card1 ? setCard2(card) : setCard2(card)
+        Card1 ? setCard2(card) : setCard1(card)
     }
     //compare
     useEffect(() => {
         if (Card1 && Card2) {
             if (Card1.name === Card2.name) {
-                console.log('match')
+                setCards(prevCard => {
+                    return prevCard.map(card => {
+                        if (card.name === Card1.name) {
+                            return ({ ...card, status: true })
+                        } else {
+                            return card
+                        }
+                    })
+                })
                 resetCard()
             } else {
-                console.log('unmatch')
-                resetCard()
+                setTimeout(() =>
+                resetCard(), 1000)
             }
         }
     }, [Card1, Card2])
@@ -45,7 +50,8 @@ function App() {
         <div className="App">
             <button onClick={shuffel}> New Game </button>
             <div className="Cards">
-                {cards.map(cardValues => (<Card key={cardValues.id} cardText={cardValues} handleChoice={handleChoice }/>))}
+                {cards.map(cardValues => (<Card key={cardValues.id} cardText={cardValues} handleChoice={handleChoice}
+                    flipped={cardValues === Card1 || cardValues === Card2 || cardValues.status} />))}
             </div>
         </div>
     );
