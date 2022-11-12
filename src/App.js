938f44978ef86316618bcfda26a95d30b4c25400
Thai Huynh <rafehuynh0510@gmail.com>
2022-11-12 14:25:55 -0500

    handle choice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Card from './Card'
+
 const cardValues = [{ "name": "Card1" },
 { "name": "Card2" },
 { "name": "Card3" },
@@ -9,19 +10,26 @@ const cardValues = [{ "name": "Card1" },
 function App() {
     const [cards, setCards] = useState([])
     const doublecards = [...cardValues, ...cardValues]
-    function resetGame() {
-        window.location.reload(false)
+    const [Card1, setCard1] = useState(null)
+    const [Card2, setCard2] = useState(null)
+    const resetGame = () => {
+        window.location.reload(true)
     }
     // shuffel the cards 
     const shuffel = () => {
         const shuffed = doublecards.sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }))
         setCards(shuffed)
     }
-    console.log(cards)
+    // handle choice for card 1 and card 2
+    const handleChoice = (card) => {
+        Card1 ? setCard2(card) : setCard2(card)
+    }
     return (
         <div className="App">
-            <button onClick={resetGame}> New Game </button>
-            {cardValues.map((item, idx) => (<Card key={idx} cardText={item} />))}
+            <button onClick={shuffel}> New Game </button>
+            <div className="Cards">
+                {cards.map(cardValues => (<Card key={cardValues.id} cardText={cardValues} handleChoice={handleChoice }/>))}
+            </div>
         </div>
     );
 }
--- src/Card.js
+++ src/Card.js
@@ -3,24 +3,24 @@ import React from 'react';
 class Card extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { isFlipped: false }
-    }
-
-    handleClick = () => {
-        this.setState(state => ({ isFlipped: !state.isFlipped }));
-    }
-
-    render() {
-        if (this.state.isFlipped === false) {
-            return (
-                <div style={{ height: '100px', width: '75px', backgroundColor: 'black', borderStyle: 'solid', borderColor: 'red' }}
-                    onClick={this.handleClick}></div>
-            );
+        this.state = {
+            isFlipped: false,
         }
-        return (
-            <div style={{ height: '100px', width: '75px', backgroundColor: 'lightblue', borderStyle: 'solid', borderColor: 'red' }}
-                onClick={this.handleClick}>{this.props.cardText}</div>
-        );
+    }
+    //handle click
+    handleClick = () => {
+        this.setState(state => ({ isFlipped: !state.isFlipped }))
+        this.props.handleChoice(this.props.cardText)
+    }
+    render() {
+        if (!this.state.isFlipped) {
+            return (
+                <div className="card" onClick={this.handleClick}></div>
+            )
+        } else {
+            return (
+                <div className='cardup'> {this.props.cardText.name} </div>);
+        }
     }
 }
 
